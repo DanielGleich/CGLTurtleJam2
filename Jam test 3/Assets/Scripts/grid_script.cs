e7b8ffe4c0d55d60280d700bef3a8767cee548b5
MishotoDraganov <42991555+MishotoDraganov@users.noreply.github.com>
2023-11-25 13:44:10 +0200

    M. Rotation

--- Jam test 3/Assets/Scripts/grid_script.cs
+++ Jam test 3/Assets/Scripts/grid_script.cs
@@ -6,29 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class grid_script : MonoBehaviour
 {
-    public Vector2[] gridSize;
     public static int currentLevel=1;
     string rotatingDirection = "none";
     int rotationCounter = 0;
-    float cellSize = 1;
 
 
     [SerializeField] GameObject Grid_Cell;
 
     void Start()
     {
-        CreateGrid();
-        CenterCameraOnObject();
-        DontDestroyOnLoad(gameObject);
-
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             currentLevel++;
-            CreateGrid();
-            CenterCameraOnObject();
         }
         if (rotatingDirection == "none")
         {
@@ -67,45 +59,6 @@ public class grid_script : MonoBehaviour
                 rotationCounter = 0;
                 rotatingDirection = "none";
             }
-        }
-    }
-
-    void CenterCameraOnObject()
-    {
-
-        foreach (Transform child in transform)
-        {
-            child.gameObject.transform.position -= new Vector3((gridSize[currentLevel].x * cellSize) / 2 - cellSize/2, (gridSize[currentLevel].y * cellSize) / 2 - cellSize/2, child.gameObject.transform.position.z);
-        }
-
-       // Vector3 targetCenter = transform.position;
-        //Camera.main.transform.position = new Vector3(targetCenter.x, targetCenter.y, transform.position.z);
-    }
-    void CreateGrid()
-    {
-        DeleteChildren();
-       if(SceneManager.GetActiveScene().name != "Level " + currentLevel) SceneManager.LoadScene("Level " + currentLevel);
-        cellSize = (10f / gridSize[currentLevel].x);
-        
-
-        for (int x = 0; x < gridSize[currentLevel].x; x++)
-        {
-            for (int y = 0; y < gridSize[currentLevel].y; y++)
-            {
-                Vector3 cellPosition = new Vector3(x*cellSize, y*cellSize, 0);
-                GameObject cell = Instantiate(Grid_Cell, cellPosition, Quaternion.identity);
-                cell.transform.parent = transform;
-                cell.transform.localScale = new Vector3(cellSize, cellSize, 1);
-
-            }
-        }
-    }
-    void DeleteChildren()
-    {
-        // Iterate through all children and destroy them
-        foreach (Transform child in transform)
-        {
-            Destroy(child.gameObject);
         }
     }
 }
