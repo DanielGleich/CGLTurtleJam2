2ea938c9d372992063829c684794952b3b7c4572
MiniDado <isabelacostarotta@gmail.com>
2023-11-26 12:12:41 +0100

    trash implementation

--- Jam test 3/Assets/Scripts/SpriteSelector.cs
+++ Jam test 3/Assets/Scripts/SpriteSelector.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class SpriteSelector : MonoBehaviour
 {
     [SerializeField] Sprite sprite;
-    SpriteRenderer spriteRenderer;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        foreach (Transform child in transform) {
-            
-            if (transform.gameObject.name.Contains("Sprite") && child.TryGetComponent<SpriteRenderer>(out spriteRenderer)) {
-                break;
-            }
-         }
         if (sprite != null && spriteRenderer != null) {
             spriteRenderer.sprite = sprite;
         }
